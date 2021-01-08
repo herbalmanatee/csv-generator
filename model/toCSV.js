6fb0ec9b54b54489c202a562550740f640b05d38
William <williameliason8@gmail.com>
2021-01-07 22:15:28 -0600

    parse data into csv

--- model/toCSV.js
+++ model/toCSV.js
@@ -3,10 +3,27 @@ const _ = require('underscore');
 let toCSV = (data) => {
   let template = _.template("<div><%- data%></div>");
   let dataObj = JSON.parse(data);
+  //let csvString = '';
+  let headerString = '';
+  let bodyString = '';
 
-  for (let key in dataObj) {
-    console.log(dataObj[key]);
+  let traverseObj = (obj) => {
+    for (let key in obj) {
+      if (key === 'children') {
+        for (let element of obj[key]) {
+          traverseObj(element);
+        }
+      } else {
+        if (!headerString.includes(key.toString())) {
+          headerString += key + ',';
+        }
+        bodyString += obj[key].toString() + ',';
+      }
+    }
+    return headerString+bodyString.slice(0,bodyString.length-1);
   }
-  return template({data: dataObj});
+  let getcsvString = traverseObj(dataObj);
+
+  return template({data: getcsvString});
 }
  export default toCSV;
